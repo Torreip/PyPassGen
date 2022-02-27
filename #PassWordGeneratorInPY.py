a9a79e9d65dac2486c6b846e24a887d768a9b026
Torreip <80983183+Torreip@users.noreply.github.com>
2022-02-27 13:32:43 +0100

    V0.6 update (start of the optimisation and the styling process)

--- #PassWordGeneratorInPY.py
+++ #PassWordGeneratorInPY.py
@@ -23,30 +23,38 @@ def options():
 
 #Generation Function And Subordinate
 def gen():
+    clear()
+    dataRead()
     global email
-    name = input("What's the service name ? ")
-    print(email)#should dispear with only 1 choice
+    sName = input("What's the service name ? ")
     email_choice = -1
-    print("what email should be used for the ",name, "site or app ?")
-    while email_choice > len(email) or email_choice < 0:
-        try:
-            email_choice = int(input())-1
-        except ValueError:
-            print("invalid input ")
+    if len(email) > 1:
+        for x in range(len(email)):
+            print(x+1,")",email[x])
+        print("what email should be used for the ",sName, "site or app ?")
+        while email_choice > len(email) or email_choice < 0:
+            try:
+                email_choice = int(input("Please enter the number associeted with the desired email."))-1
+            except ValueError:
+                print("invalid input ")
     chars = "#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';"#MAIN charslist.
     chartest = list(chars)
-    pwd = random.choices(chartest, k=16) #change lenght of pass here
-    pyperclip.copy("".join(pwd))
-    filePwd()
-    
-def filePwd():
-    pass#addition to a file.
+    pwd = "".join(random.choices(chartest, k=16)) #change lenght of pass here
+    print(pwd)
+    pyperclip.copy(pwd)
+    pwd_data = open("pwd.txt", "a+")
+    pwd_data.write("--- "+sName+" ---\n")  #MODIFY HERE THE FILE STRUCTURE
+    pwd_data.write("\nemail : "+email[1+email_choice]+"\n")
+    pwd_data.write("password : "+pwd+"\n")
+    pwd_data.write("\n")
 
+#File Functions:
 def dataRead():
     global email
     try:
         data_email = open("data.dbeml", "r")
         email = data_email.readlines()
+        email = [x[:-1] for x in email]
         data_email.close
         if email:
             pass#may do something here to tell the user ^^
@@ -72,6 +80,7 @@ def emailAdd():
     data_email.close
 
 def dataFill():
+    clear()
     global email
     if email == 0:
         print("Let's populate that file with it's first data !")
@@ -97,46 +106,43 @@ def dataFill():
                 c2 = input("Do you want to continue (Yes or no) ? ")
                 c2 = c2.lower()
             else:
-                clear()
                 menu()
 
 #Menu Fonction And Subordinate
 def menu():
-    print("     [1] Generate a Password")
+    clear()
+    print("\n\n     [1] Generate a Password")
     print("     [2] Populate The email file")
     print("     [3] Manipulate The email file")
     print("     [4] Options")
     print("     [5] Info")
     print("     [6] Quit")
-    print("\33[1;31;31m     [9] Delete The email data file \33[0m")
+    print("\33[1;31;31m     [9] Delete The email data file \33[0m\n\n")
     #WIP Thinking about adding a 5th an 6th menu opytion (Delete a file and manipulate the file itself changing order delete ect)
 
     while True:
         try: 
-            c1 = int(input("Make your choice (between 1 and x or 9) "))
+            c1 = int(input("Please type the number associated with your choice. "))
         except ValueError:
-            print("invalid input, please select a number between 1 and x or 9. ")
+            print("invalid input, please select a valid option from the above menu. ")
         else:
             if c1 == 1:
                 gen()
             elif c1 == 2:
                 dataFill()
             elif c1 == 3:
-                print("WIP WIP WIP MANIPULATION OF DATA FILE")#WIP
+                pass
             elif c1 == 4:
-                options()#WIP
+                options()#
             elif c1 == 5:
                 print("This python script as for utility to automaticly create and store password into a text file\nIt use the storing method the creator use tho you can modify this script as much as you want to better fit you.")
                 print("Signed Torreip 2022")
-                time.sleep(7.5)
-                clear()
+                input("Press enter to return to the menu ")
                 menu()
             elif c1 == 6:
-                print("Thank's for using my script !")
-                time.sleep(2)
-                sys.exit()
+                exit()
             elif c1 == 9:
-                print("\x1b[1;31;31mThis Action Will Delete every bit of data in the data.dbeml file. This can't be reversed\x1b[0m")
+                print("\33[1;31;31mThis Action Will Delete every bit of data in the data.dbeml file. This can't be reversed\33[0m")
                 try: 
                     c3 = input("Make your choice (Yes or no) ")
                 except ValueError:
@@ -147,22 +153,35 @@ def menu():
                         if os.path.exists("data.dbeml"):
                             print("\33[1;31;31mNUKE PROCEDURE ENGAGED\33[0m")
                             os.remove("data.dbeml")
-                            print("Thank's for using my script !")
-                            time.sleep(2)
-                            sys.exit()
+                            exit()
                         else:
                             print("An error occured the program will now close please restart it and select the 9th option again.")
                             time.sleep(5)
                             sys.exit
                     else:
-                        clear()
                         menu()
             elif c1 == 507444126:
                 print("Even tho the option wasn't presented you found it !")
                 print("I congratulate you, you found this easter egg.")
                 print("Torreip")
+                time.sleep(5)
+                menu()
+
+def exit():
+    print("\33[94mThank's for using my script ! (V0.6)")
+    time.sleep(2)
+    sys.exit()
+
+
+
+
+
+#TODO:
 
 #TO IMPROVE REDABILITY ==> Switch menu to case statement.
+#Finish Every WIP feature
+#Order the code!
+#V1.0 release ??
 
 #General Code
 
